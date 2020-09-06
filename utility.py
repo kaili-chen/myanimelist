7bc29464cc3c0c5466ca3b6e5459f5be01dbfc87
KaiLi Chen <kailichen@KaiLis-MacBook-Pro.local>
2020-09-07 00:16:34 +0800

    added save_json function

--- utility.py
+++ utility.py
@@ -9,6 +9,8 @@ get_soup(url -> string): returns b24.BeautifulSoup object from url
 import sys
 import requests
 from bs4 import BeautifulSoup
+import os
+import json
 
 ### UTILITY FUNCTIONS
 def get_soup(url):
@@ -33,5 +35,29 @@ def get_soup(url):
 
     return soup
 
+def save_json(data, filename):
+    '''
+    Saves data to a json file.
 
-# TODO: add function to remove children from bs4 parent element
+    Parameters:
+        data (dict): data to save to json file
+        filename (string): filename to save json file as (saves to current dir by default)
+
+    Returns:
+        path (string): absolute path to json file
+    '''
+
+    # TODO: use path_out instead of filename instead?
+
+    # checks if file already has json extension
+    if ".json" not in filename:
+        filename = "{}.json".format(filename)
+
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4)
+
+    return os.path.abspath(filename)
+
+def remove_children():
+    # TODO: add function to remove children from bs4 parent element
+    pass
